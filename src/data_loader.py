d033f56a4214b82adf95d8040de9f6c264fea6c8
lmiadowicz <l.miadowicz@gmail.com>
2024-05-12 23:24:35 +0200

    feat: add FileNotFoundError exception

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,13 @@
 import pandas as pd
+from src.logger import logger
 
 
 def load_urls_from_tsv(file_path):
-    df = pd.read_csv(file_path, delimiter='\t', dtype=str)
+    logger.info(f"Loading URLs from file: {file_path}")
+    try:
+        df = pd.read_csv(file_path, delimiter='\t', dtype=str)
+    except FileNotFoundError as e:
+        logger.error(f"File not found: {file_path}, error: {e}")
+        raise
 
     return df[['url']]
