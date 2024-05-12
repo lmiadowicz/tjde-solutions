31cef178246ee5584007ff50151592d89aaa4aec
lmiadowicz <l.miadowicz@gmail.com>
2024-05-12 23:14:50 +0200

    feat: add data_loader & logger

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,2 +1,16 @@
-def test_empty():
-    pass
+import pandas as pd
+from src.data_loader import load_urls_from_tsv
+from py.path import LocalPath
+
+
+def test_load_urls_valid_file(tmpdir: LocalPath) -> None:
+    file_path = tmpdir / "test_input.tsv"
+    with open(file_path, "w") as f:
+        f.write("url\n")
+        f.write("https://www.tidio.com/?a_bucket=bucket1&a_type=type1&a_source=source1\n")
+
+    df = load_urls_from_tsv(file_path)
+
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 1
+    assert df['url'].iloc[0] == "https://www.tidio.com/?a_bucket=bucket1&a_type=type1&a_source=source1"
