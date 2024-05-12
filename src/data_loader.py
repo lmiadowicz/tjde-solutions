02db3933684c506b5a05189337b40cbac652e9a1
lmiadowicz <l.miadowicz@gmail.com>
2024-05-12 23:36:51 +0200

    feat(data-loader): added docstring

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,8 +2,22 @@ import pandas as pd
 from src.logger import logger
 
 
-def load_urls_from_tsv(file_path):
+def load_urls_from_tsv(file_path: str) -> pd.DataFrame:
+    """Loads URLs from a TSV input file and returns a DataFrame.
+
+    Args:
+        file_path (str): The path to the TSV file.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing URLs.
+
+    Raises:
+        FileNotFoundError: If the specified file is not found.
+        pd.errors.EmptyDataError: If the file is empty or has no columns to parse.
+        ValueError: If the TSV file doesn't have a 'url' column.
+    """
     logger.info(f"Loading URLs from file: {file_path}")
+
     try:
         df = pd.read_csv(file_path, delimiter='\t', dtype=str)
     except FileNotFoundError as e:
