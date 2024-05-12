dcb8ea2f46844107033b34a8c058a1277ad89d99
lmiadowicz <l.miadowicz@gmail.com>
2024-05-13 01:33:55 +0200

    feat(output-writer): writting results to tsv file t1_output.tsv in data folder

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from data_loader import load_urls_from_tsv
 from url_parser import parse_urls
+from output_writer import write_results_to_tsv
 
 
 def main():
     file_path_input = f"data/task1_input.tsv"
+    file_path_output = f"data/task1_output.tsv"
 
     urls = load_urls_from_tsv(file_path_input)
     parsed_data = parse_urls(urls)
+    write_results_to_tsv(file_path_output, parsed_data)
 
 
 if __name__ == "__main__":
---
+++ src/output_writer.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from src.logger import logger
+
+
+def write_results_to_tsv(file_path, data):
+    """Writes parsed URL data to a TSV file.
+
+    Args:
+        file_path (str): The path to the output TSV file.
+        data (pandas.DataFrame): A DataFrame containing parsed URL data.
+
+    Raises:
+        TypeError: If the input data is not a pandas DataFrame
+        IOError: If there's an error writing to the file.
+    """
+    logger.info(f"Writing results to file: {file_path}")
+
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError("Input data must be a pandas DataFrame")
+
+    try:
+        data.to_csv(file_path, sep='\t', index=False)
+    except IOError as e:
+        logger.error(f"Error writing to file: {e}")
+        raise
+
+    logger.info(f"Successfully wrote {len(data)} rows to {file_path}")  # Log number of rows
